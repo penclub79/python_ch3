14d607dbd0d457e87fe1b70f724baac3705d0544
penclub79 <penclub79@naver.com>
2018-06-27 18:28:30 +0900

    ㅎㅎ

---
+++ emaillist/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from emaillist.models import Emaillist
+# Create your views here.
+
+def index(request):
+    emaillist_list = Emaillist.objects.all().order_by('-id')
+    data = {'emaillist_list': emaillist_list}
+    return render(request, 'index.html', data)
+
+def form(request):
+    return render(request, 'form.html')
+
+def add(request):
+    emaillist = Emaillist()
+    emaillist.first_name = request.POST['fn']
+    emaillist.last_name = request.POST['ln']
+    emaillist.email = request.POST['email']
+
+    emaillist.save()
+
+    return HttpResponseRedirect('/emaillist')
