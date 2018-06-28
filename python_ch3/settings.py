8fd91e5ce68148fa6364294c26e7ed07bf796aaf
penclub79 <penclub79@naver.com>
2018-06-28 15:58:29 +0900

    ㅎㅎ

--- python_ch3/settings.py
+++ python_ch3/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'emaillist'
+    'mathfilters',
+    'emaillist',
+    'guestbook'
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'python_ch3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- python_ch3/urls.py
+++ python_ch3/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from emaillist import views
+import emaillist.views as emaillist_views
+import guestbook.views as guestbook_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('emaillist/', views.index),
-    path('emaillist/form', views.form),
-    path('emaillist/add', views.add)
 
+    path('emaillist/', emaillist_views.index),
+    path('emaillist/form', emaillist_views.form),
+    path('emaillist/add', emaillist_views.add),
+
+    path('guestbook/', guestbook_views.index),
+    path('guestbook/add', guestbook_views.add),
+    path('guestbook/deleteform', guestbook_views.delete)
 ]
